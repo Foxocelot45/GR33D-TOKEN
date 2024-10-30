79916c6f6097a17734619e160df91bfce0f9da1e
Foxocelot45 <label.vanguard@gmail.com>
2024-10-30 12:23:41 +0100

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page-content">
+      <h2>Home</h2>
+      <p>Page en construction.</p>
+    </div>
+  );
+}
+
+export default Home;
