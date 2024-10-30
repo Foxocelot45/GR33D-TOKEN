caeb0c653045360fb5da1c9e542d800df8672043
FOX-OCELOT <label.vanguard@gmail.com>
2024-10-30 16:20:14 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
   return (
-    <div className="page-content">
-      <h2>Home</h2>
-      <p>Page en construction.</p>
+    <div className="home-container">
+      <h1 className="home-text">
+        Web3. Security. Transparency. Efficiency.
+        <br />
+        By community, for community.
+        <br />
+        <span>Join the future. Join the Greedy's Club ecosystem.</span>
+      </h1>
     </div>
   );
 }
