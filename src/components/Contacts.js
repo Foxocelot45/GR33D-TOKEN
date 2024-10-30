08698d81113be6306a410cd873ea8194167ae4c4
Foxocelot45 <label.vanguard@gmail.com>
2024-10-30 12:24:42 +0100

    Create Contacts.js

---
+++ src/components/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contacts() {
+  return (
+    <div className="page-content">
+      <h2>Contacts</h2>
+      <p>Page en construction.</p>
+    </div>
+  );
+}
+
+export default Contacts;
