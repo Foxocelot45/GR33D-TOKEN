66d4c757aa68bc3708ff8c25b71327916d70c488
Foxocelot45 <label.vanguard@gmail.com>
2024-10-30 12:24:07 +0100

    Create Roadmap.js

---
+++ src/components/Roadmap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Roadmap() {
+  return (
+    <div className="page-content">
+      <h2>Roadmap</h2>
+      <p>Page en construction.</p>
+    </div>
+  );
+}
+
+export default Roadmap;
