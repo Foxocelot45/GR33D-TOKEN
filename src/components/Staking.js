0b3f92eb2eae45850df6650a65cf49966aba1d04
Foxocelot45 <label.vanguard@gmail.com>
2024-10-30 12:24:25 +0100

    Create Staking.js

---
+++ src/components/Staking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Staking() {
+  return (
+    <div className="page-content">
+      <h2>Staking</h2>
+      <p>Page en construction.</p>
+    </div>
+  );
+}
+
+export default Staking;
