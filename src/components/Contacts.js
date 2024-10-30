b690df9bbb0d76a2c3400cb78a4da4ffcac517f6
FOX-OCELOT <label.vanguard@gmail.com>
2024-10-30 16:08:39 +0100

    Update Contacts.js

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,15 +1,18 @@
+// Contacts.js
 import React from 'react';
 import './Contacts.css';
 
 function Contacts() {
   return (
-    <div className="contacts-container">
-      <h2>Contacts</h2>
-      <p>Retrouvez-nous sur nos réseaux sociaux :</p>
-      <div className="social-links">
-        <a href="https://x.com/TheGr33dyzClub" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://t.co/e6Z9lyHC2k" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://t.me/+XWXpQfDJoUhmZDlk" target="_blank" rel="noopener noreferrer">Telegram</a>
+    <div className="contacts-section">
+      <div className="contacts-content">
+        <h2>Contacts</h2>
+        <p>Retrouvez-nous sur nos réseaux sociaux :</p>
+        <div className="social-links">
+          <a href="https://x.com/TheGr33dyzClub" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://t.co/e6Z9lyHC2k" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://t.me/+XWXpQfDJoUhmZDlk" target="_blank" rel="noopener noreferrer">Telegram</a>
+        </div>
       </div>
     </div>
   );
