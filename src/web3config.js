e93c71b4b4b4ed987f96e0c160790d94b9691d7d
FOX-OCELOT <label.vanguard@gmail.com>
2024-10-31 11:27:27 +0100

    Update web3config.js

--- src/web3config.js
+++ src/web3config.js
@@ -3,18 +3,18 @@ import { ethers } from 'ethers';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 
 const providerOptions = {
-  walletconnect: {
-    package: WalletConnectProvider,
-    options: {
-      infuraId: "ba7fccd6b6004ecfbcf47ae9f61578ff", // Remplacez par votre Infura ID
-    },
-  },
   injected: {
     display: {
       name: "Metamask",
       description: "Connect with the Metamask wallet extension",
     },
     package: null,
+  },
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: "ba7fccd6b6004ecfbcf47ae9f61578ff",
+    },
   },
 };
 
@@ -28,7 +28,6 @@ export async function connectWallet() {
   const instance = await web3Modal.connect();
   const provider = new ethers.providers.Web3Provider(instance);
 
-  // Récupérer l'adresse du compte de manière recommandée
   const accounts = await provider.send("eth_accounts", []);
   const userAddress = accounts[0];
 
