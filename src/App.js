a26932870e4fb2efa15bc6f57c85068c481b77f0
FOX-OCELOT <label.vanguard@gmail.com>
2024-10-31 09:46:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,18 @@ import Roadmap from './components/Roadmap';
 import Staking from './components/Staking';
 import Contacts from './components/Contacts';
 import Whitepaper from './components/Whitepaper';
-import logoImage from './assets/The-Greedy-s-Club-31-10-2024.png';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header>
-          <img src={logoImage} alt="The Greedy's Club Logo" className="header-logo" />
+          <h1 className="header-title">The Greedy's Club</h1>
           <nav>
             <Link to="/">Home</Link>
             <Link to="/about">About Us</Link>
             <Link to="/roadmap">Roadmap</Link>
-            <Link to="/whitepaper">Whitepaper</Link>
+            <Link to="/whitepaper" target="_blank" rel="noopener noreferrer">Whitepaper</Link>
             <Link to="/dex" className="disabled">DEX/AMM (Soon)</Link>
             <Link to="/staking">Staking</Link>
             <Link to="/contacts">Contacts</Link>
