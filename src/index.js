a0c105e15003161b760ad53bd1847f176ebb1777
FOX-OCELOT <label.vanguard@gmail.com>
2024-11-02 16:59:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
+import Web3Provider from './components/Web3Provider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3Provider>
+      <App />
+    </Web3Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
