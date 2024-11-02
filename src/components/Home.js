812bdef33c0e122c688ae1ec3b201d3294b96dc2
FOX-OCELOT <label.vanguard@gmail.com>
2024-11-02 15:37:56 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 
 function Home() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const homeText = document.querySelector('.home-text');
+      if (homeText) {
+        const position = homeText.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (position < windowHeight - 100) {
+          homeText.classList.add('scrolled');
+        }
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="home-container">
-      <h1 className="home-text">
+      <h1 className="home-text scroll-reveal">
         Web3. Security. Transparency. Efficiency.
         <br />
         By community, for community.
